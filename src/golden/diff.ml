7d4fedf75108233d08f3b81dd1fb36dbb5413cf9
Jonathan Coates <git@squiddev.cc>
2020-03-06 10:53:01 +0000

    Remove our dependency on patience_diff
    This replaces it with our home-grown diff implementation. While this is
    objectively worse, it saves us dragging in the entirety of core_kernel.

---
+++ src/golden/diff.ml
@@ -0,0 +1,83 @@
+(** A port of simplediff[1] to OCaml. This is an incredibly poor diff implementation (missing patch
+    support), terrible complexity, but hopefully it's good enough for the time being.
+
+    [1]: https://github.com/paulgb/simplediff *)
+module Slice = struct
+  type 'a t =
+    { array : 'a array;
+      start : int;
+      length : int
+    }
+
+  let of_array array = { array; start = 0; length = Array.length array }
+
+  let length x = x.length
+
+  let iter f { array; start; length } =
+    for i = 0 to length - 1 do
+      f array.(start + i)
+    done
+
+  let iteri f { array; start; length } =
+    for i = 0 to length - 1 do
+      f i array.(start + i)
+    done
+
+  let slice ~from ~length { array; start; _ } = { array; start = start + from; length }
+end
+
+type 'a diff =
+  | Add of 'a Slice.t
+  | Remove of 'a Slice.t
+  | Keep of 'a Slice.t
+  | Seq of 'a diff list
+
+let rec diff olds news =
+  match (Slice.length olds, Slice.length news) with
+  | 0, 0 -> Seq []
+  | 0, _ -> Add news
+  | _, 0 -> Remove news
+  | _, _ ->
+      (* Build up a map of all lines contents to their positions. *)
+      let old_index_map = Hashtbl.create (Slice.length olds / 2) in
+      Slice.iteri (fun i x -> Hashtbl.add old_index_map x i) olds;
+
+      let overlap = Array.make (Slice.length olds) 0 in
+      let sub_length = ref 0 and sub_range = ref (0, 0) in
+
+      news
+      |> Slice.iteri (fun new_index value ->
+             let this_overlap = Array.make (Slice.length olds) 0 in
+             Hashtbl.find_all old_index_map value
+             |> List.iter (fun old_index ->
+                    let this = if old_index = 0 then 1 else overlap.(old_index - 1) + 1 in
+                    this_overlap.(old_index) <- this;
+                    if this > !sub_length then (
+                      sub_length := this;
+                      sub_range := (old_index - this + 1, new_index - this + 1) )));
+
+      if !sub_length = 0 then Seq [ Remove olds; Add news ]
+      else
+        let old_idx, new_idx = !sub_range and length = !sub_length in
+        Seq
+          [ diff
+              (Slice.slice ~from:0 ~length:old_idx olds)
+              (Slice.slice ~from:0 ~length:new_idx news);
+            Keep (Slice.slice ~from:new_idx ~length news);
+            diff
+              (Slice.slice ~from:(old_idx + length)
+                 ~length:(Slice.length olds - old_idx - length)
+                 olds)
+              (Slice.slice ~from:(new_idx + length)
+                 ~length:(Slice.length news - new_idx - length)
+                 news)
+          ]
+
+let fold ~add ~remove ~keep diff =
+  let rec go = function
+    | Add x -> add x
+    | Remove x -> remove x
+    | Keep x -> keep x
+    | Seq xs -> List.iter go xs
+  in
+  go diff
--- src/golden/omnomnomGolden.ml
+++ src/golden/omnomnomGolden.ml
@@ -25,25 +25,12 @@ let read_all channel =
   close_in channel; Buffer.contents buffer
 
 let display_diff diff out =
-  let module Diff = Patience_diff_lib.Patience_diff in
   let module F = Omnomnom.Formatting in
   (* The eta-expansion here is important, otherwise the formatting codes are only emitted once. *)
-  let same x = Format.fprintf out " %s@\n" x
-  and minus x = F.printf F.(DullColor Red) out "-%s@\n" x
-  and plus x = F.printf F.(DullColor Green) out "+%s@\n" x in
-  diff
-  |> List.iter (fun (hunk : string Diff.Hunk.t) ->
-         F.printf
-           F.(DullColor Cyan)
-           out "%@%@ -%i,%i +%i,%i %@%@@\n" hunk.prev_start hunk.prev_size hunk.next_start
-           hunk.next_size;
-         hunk.ranges
-         |> List.iter (function
-              | Diff.Range.Same xs -> xs |> Array.iter (fun (x, _) -> same x)
-              | Prev xs -> Array.iter minus xs
-              | Next xs -> Array.iter plus xs
-              | Replace (prev, next) -> Array.iter minus prev; Array.iter plus next
-              | Unified xs -> Array.iter same xs))
+  let keep = Diff.Slice.iter (fun x -> Format.fprintf out " %s@\n" x)
+  and remove = Diff.Slice.iter (fun x -> F.printf F.(DullColor Red) out "-%s@\n" x)
+  and add = Diff.Slice.iter (fun x -> F.printf F.(DullColor Green) out "+%s@\n" x) in
+  Diff.fold ~keep ~add ~remove diff
 
 let compare ~regenerate ~directory ~output_name ~output_actual =
   let output_path = Filename.concat directory output_name in
@@ -53,13 +40,10 @@ let compare ~regenerate ~directory ~output_name ~output_actual =
   else
     (* Generate a diff and pretty-print it. This is horrible, and really should be using some
        pretty-printing library. *)
-    let module Diff = Patience_diff_lib.Patience_diff in
     let diff =
-      Diff.String.get_hunks
-        ~transform:(fun x -> x)
-        ~context:3 ?big_enough:None
-        ~prev:(String.split_on_char '\n' output_expected |> Array.of_list)
-        ~next:(String.split_on_char '\n' output_actual |> Array.of_list)
+      Diff.diff
+        (String.split_on_char '\n' output_expected |> Array.of_list |> Diff.Slice.of_array)
+        (String.split_on_char '\n' output_actual |> Array.of_list |> Diff.Slice.of_array)
     in
     (* Regenerate the file if needed .*)
     let () =
@@ -97,21 +81,15 @@ let of_file action ~directory ~input_name ~output_name =
 let out x = x ^ ".out"
 
 let of_directory action ?group:group_name ?(rename = out) ~directory ~extension () =
-  try
-    let names = ref [] in
-    let handle = Unix.opendir directory in
-    ( try
-        while true do
-          let child = Unix.readdir handle in
-          if child <> "." && child <> ".." && Filename.extension child = extension then
-            names := child :: !names
-        done
-      with End_of_file -> () );
-    !names |> List.sort String.compare
-    |> List.map (fun child ->
-           of_file action ~directory ~input_name:child
-             ~output_name:(Filename.remove_extension child |> rename))
-    |> group (Option.value group_name ~default:directory)
-  with e ->
-    let res = result_of_exn e in
-    test directory (fun () -> res)
+  match Sys.readdir directory with
+  | names ->
+      Array.to_list names
+      |> List.filter (fun c -> Filename.extension c = extension)
+      |> List.sort String.compare
+      |> List.map (fun child ->
+             of_file action ~directory ~input_name:child
+               ~output_name:(Filename.remove_extension child |> rename))
+      |> group (Option.value group_name ~default:directory)
+  | exception e ->
+      let res = result_of_exn e in
+      test directory (fun () -> res)
